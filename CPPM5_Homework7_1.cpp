df9a7aa8baf8180f323ba9d107c9289f5cd16e04
Anton Brylyakov <yoll@rambler.ru>
2022-10-01 20:24:20 -0700

    Начальный коммит

---
+++ CPPM5_Homework7_1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+#define MODE 1
+
+#ifndef MODE
+#error "Макрос MODE необходим для компиляции программы"
+#endif
+
+#if MODE == 1
+
+namespace IntegerArithmetic
+{
+	int add(int x, int y)
+	{
+		return x + y;
+	}
+}
+
+#endif
+
+int main()
+{
+
+	setlocale(LC_ALL, "Russian");
+
+#if MODE == 0
+
+	std::cout << "Работаю в режиме тренировки" << std::endl;
+
+#elif MODE == 1
+
+	int x, y, res;
+	std::cout << "Работаю в боевом режиме" << std::endl;
+	std::cout << "Введите число 1: ";
+	std::cin >> x;
+	std::cout << "Введите число 2: ";
+	std::cin >> y;
+	res = IntegerArithmetic::add(x, y);
+	std::cout << "Результат сложения: " << res << std::endl;
+
+#else
+	std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+
+#endif
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Задача 1. Препроцессорные директивы
+[Условие задачи](https://github.com/netology-code/cppm-homeworks/tree/main/07/01)
